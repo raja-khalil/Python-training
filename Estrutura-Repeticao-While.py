b35390ea98d4f3b9482059a90b049b50f030acfc
Raja KHalil <rajakhalil.acedevt@gmail.com>
2025-02-07 19:15:04 -0300

    Estrutura em Python

---
+++ Estrutura-Repeticao-While.py
@@ -0,0 +1,9 @@
+#Estrutura-Repeticao-While
+
+#loop utilizando while
+
+contador = 0 
+
+while contador <= 10: 
+	print(f'valor do contador é {contador}')
+	contador +=1
---
+++ Estrutura-condicionais.py
@@ -0,0 +1,30 @@
+# Estrutura Condicional Simples
+
+#soma1 = 3 
+
+#if soma > 0:
+    #print("maior do que zero")
+#else:
+    #print("Menor do que zero")
+
+# Outro exemplo
+
+#numero1 = 2
+#numero2 = 3 
+
+#if numero1 == numero2: 
+    #print ("valores são iguais")
+#else:
+    #print("os valores são diferentes")
+
+# estrutura Condicional aninhada
+
+#soma=1
+
+#if soma > 0: 
+    #print("Soma maior que zero")
+#elif soma == 0:
+   # print("Soma é igual a zero")
+#else:
+   # print("soma é menor do que zero")
+   
